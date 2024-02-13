0221200a1acb694253519e6a7d9767b47737333e
00marina <marina.maric.b@gmail.com>
2024-02-13 11:37:42 +0100

    initial app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoViewComponent } from './todo-view/todo-view.component';
+import { AddNewTodoComponent } from './add-new-todo/add-new-todo.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
+import { CategoryViewComponent } from './category-view/category-view.component';
+import { CategoryNewComponent } from './category-new/category-new.component';
+import { CategoryEditComponent } from './category-edit/category-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoViewComponent,
+    AddNewTodoComponent,
+    EditTodoComponent,
+    CategoryViewComponent,
+    CategoryNewComponent,
+    CategoryEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-view/todo-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-view',
+  templateUrl: './todo-view.component.html',
+  styleUrl: './todo-view.component.css'
+})
+export class TodoViewComponent {
+
+}
