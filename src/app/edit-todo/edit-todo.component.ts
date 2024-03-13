17c05c19ab8aaee9aad071e300e624fa88c1d055
00marina <marina.maric.b@gmail.com>
2024-03-13 17:31:03 +0100

    almost done

--- src/app/edit-todo/edit-todo.component.ts
+++ src/app/edit-todo/edit-todo.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Area } from '../area';
+import { TodoServiceService } from '../todo-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-todo',
@@ -6,5 +11,73 @@ import { Component } from '@angular/core';
   styleUrl: './edit-todo.component.css'
 })
 export class EditTodoComponent {
+  theTask: any;
+  populated: boolean = false;
+  areaId: any;
+
+  editTaskForm: FormGroup;
+
+  constructor(
+    private service: TodoServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.editTaskForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required),
+      category: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit() {
+    this.areaId = this.route.snapshot.paramMap.get('id');
+    console.log('ID:', this.areaId);
+
+    this.isPopulated();
+
+    this.service.getById(parseInt(this.areaId)).subscribe((data: any) => {
+      console.log("recieved data: ", data);
+      this.theTask = data;
+      console.log("the task: ", this.theTask);
+      this.populateForm();
+    })
+  }
+
+  // check if 
+  isPopulated() {
+    if (this.theTask !== null) {
+      this.populated = true;
+    } else {
+      this.populated = false;
+    }
+  }
+
+  populateForm () {
+    console.log("populate form was called!");
+    this.editTaskForm.patchValue({
+      name: this.theTask[0].name,
+      desc: this.theTask[0].desc,
+      category: this.theTask[0].category,
+    });
+  }
+
+  
+  goHome() {
+    this.router.navigate(['/home']);
+  }
+
+  onSubmit() {
+    this.theTask = {
+      id: this.areaId,
+      name: this.editTaskForm.value.name,
+      desc: this.editTaskForm.value.desc,
+      category: this.editTaskForm.value.category,
+      done: this.theTask[0].done,
+    }
+    console.log("sent data: ", this.theTask);
+
+    this.service.edit(this.theTask);
+    this.goHome();
+  }
 
 }
--- src/app/todo-service.service.ts
+++ src/app/todo-service.service.ts
@@ -3,7 +3,7 @@ import { Area } from './area';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoServiceService {
   demoArray: Area[] = [
@@ -12,229 +12,252 @@ export class TodoServiceService {
       name: 'Complete Angular tutorial',
       desc: 'Finish the Angular tutorial on Udemy',
       category: 'Learning',
-      done: false
+      done: false,
     },
     {
       id: 2,
       name: 'Buy groceries',
       desc: 'Milk, eggs, bread, fruits, vegetables',
       category: 'Shopping',
-      done: false
+      done: false,
     },
     {
       id: 3,
       name: 'Call Mom',
       desc: 'Wish her a happy birthday',
       category: 'Personal',
-      done: false
+      done: false,
     },
     {
       id: 4,
       name: 'Finish project report',
       desc: 'Due on Friday',
       category: 'Work',
-      done: false
+      done: false,
     },
     {
       id: 5,
       name: 'Go for a run',
       desc: '5 miles at the park',
       category: 'Health',
-      done: true
+      done: true,
     },
     {
       id: 6,
       name: 'Read book',
       desc: 'Chapter 7 to 10',
       category: 'Personal',
-      done: true
+      done: true,
     },
     {
       id: 7,
       name: 'Attend team meeting',
       desc: 'Discuss project updates',
       category: 'Work',
-      done: true
+      done: true,
     },
     {
       id: 8,
       name: 'Pay electricity bill',
       desc: 'Due date: 12th March',
       category: 'Bills',
-      done: false
+      done: false,
     },
     {
       id: 9,
       name: 'Clean the house',
       desc: 'Sweep, mop, and dust',
       category: 'Home',
-      done: false
+      done: false,
     },
     {
       id: 10,
       name: 'Practice guitar',
       desc: 'Learn new chords',
       category: 'Hobby',
-      done: false
+      done: false,
     },
     {
       id: 11,
       name: 'Plan weekend getaway',
       desc: 'Research destinations and book accommodation',
       category: 'Travel',
-      done: false
+      done: false,
     },
     {
       id: 12,
       name: 'Attend yoga class',
       desc: 'Relax and rejuvenate',
       category: 'Health',
-      done: false
+      done: false,
     },
     {
       id: 13,
       name: 'Submit expense report',
       desc: 'Include receipts for reimbursement',
       category: 'Work',
-      done: false
+      done: false,
     },
     {
       id: 14,
       name: 'Volunteer at local shelter',
       desc: 'Help with feeding and cleaning',
       category: 'Community',
-      done: false
+      done: false,
     },
     {
       id: 15,
       name: 'Research investment options',
       desc: 'Explore stocks and mutual funds',
       category: 'Finance',
-      done: false
+      done: false,
     },
     {
       id: 16,
       name: 'Update resume',
       desc: 'Highlight recent achievements',
       category: 'Career',
-      done: false
+      done: false,
     },
     {
       id: 17,
       name: 'Renew gym membership',
       desc: 'Check for any offers',
       category: 'Health',
-      done: true
+      done: true,
     },
     {
       id: 18,
       name: 'Create monthly budget',
       desc: 'Track expenses and savings goals',
       category: 'Finance',
-      done: false
+      done: false,
     },
     {
       id: 19,
       name: 'Fix leaking faucet',
       desc: 'Call plumber for appointment',
       category: 'Home',
-      done: false
+      done: false,
     },
     {
       id: 20,
       name: 'Practice meditation',
       desc: '10 minutes daily for mindfulness',
       category: 'Personal',
-      done: true
+      done: true,
     },
     {
       id: 21,
       name: 'Attend photography workshop',
       desc: 'Learn composition and lighting techniques',
       category: 'Art',
-      done: false
+      done: false,
     },
     {
       id: 22,
       name: 'Donate old clothes',
       desc: 'Pack and drop off at donation center',
       category: 'Charity',
-      done: false
+      done: false,
     },
     {
       id: 23,
       name: 'Research sustainable living practices',
       desc: 'Reduce carbon footprint',
       category: 'Environment',
-      done: false
+      done: false,
     },
     {
       id: 24,
       name: 'Cook a new recipe',
       desc: 'Experiment with flavors',
       category: 'Culinary',
-      done: true
+      done: true,
     },
     {
       id: 25,
       name: 'Attend language exchange meetup',
       desc: 'Practice speaking Spanish',
       category: 'Language',
-      done: false
+      done: false,
     },
     {
       id: 26,
       name: 'Create a vision board',
       desc: 'Visualize goals and aspirations',
       category: 'Growth',
-      done: true
+      done: true,
     },
     {
       id: 27,
       name: 'Join a local sports team',
       desc: 'Meet new people and stay active',
       category: 'Recreation',
-      done: false
+      done: false,
     },
     {
       id: 28,
       name: 'Visit a museum',
       desc: 'Explore exhibits on history and culture',
       category: 'Leisure',
-      done: false
+      done: false,
     },
     {
       id: 29,
       name: 'Plant a vegetable garden',
       desc: 'Grow your own produce',
       category: 'Gardening',
-      done: false
+      done: false,
     },
     {
       id: 30,
       name: 'Take a dance class',
       desc: 'Learn salsa or tango',
       category: 'Dance',
-      done: true
-    }        
+      done: true,
+    },
   ];
   uniqueCategoriesSet = new Set();
   categoriesArray: any[] = [];
-  
-
 
   constructor() {
-    this.demoArray.forEach(item => {
+    this.demoArray.forEach((item) => {
       this.uniqueCategoriesSet.add(item.category);
     });
     this.categoriesArray = Array.from(this.uniqueCategoriesSet);
-   }
+  }
 
+  // get all array
   getDemoArray(): Observable<Area[]> {
     return of(this.demoArray);
   }
 
-  getCategories(): Observable<any[]>{
+  // get all categories
+  getCategories(): Observable<any[]> {
     return of(this.categoriesArray);
+  }
+
+  // get by ID
+  getById(id: number): Observable<any[]> {
+    let theitem = this.demoArray.filter((item) => item.id === id);
+    console.log('from services: ', theitem);
+    return of(theitem);
+  }
+
+  //edit
+  edit(editedTask: Area) {
+    console.log("before index:", editedTask.id);
+    let index = this.demoArray.findIndex((task) => Number(task.id) === +editedTask.id);
+    console.log("recieved data in service: ", editedTask);
+    console.log("index: ", index);
+    this.demoArray[index] = {
+      id: editedTask.id,
+      name: editedTask.name,
+      desc: editedTask.desc,
+      category: editedTask.category,
+      done: editedTask.done,
+    };
+    console.log("demo array in service after: ", this.demoArray);
   }
 }
